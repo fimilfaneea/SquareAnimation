55590bbc3b0bb4f3978c954050cda5870c231bbb
Fimil Faneea <fimilfaneea@gmail.com>
2024-11-09 11:27:22 +0530

    Square animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,213 +8,153 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  HomePageState createState() => HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-  final List<IconData> icons = [
-    Icons.person,
-    Icons.message,
-    Icons.call,
-    Icons.camera,
-    Icons.file_copy_sharp,
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const SizedBox.shrink(),
-      ),
-      body: Center(
-        child: Dock(
-          items: icons,
-          onReorder: _onReorder,
+      home: Scaffold(
+        body: Center(
+          child: RedSquare(),
         ),
       ),
     );
   }
-
-  void _onReorder(int oldIndex, int newIndex) {
-    setState(() {
-      if (newIndex > oldIndex) newIndex -= 1;
-      final item = icons.removeAt(oldIndex);
-      icons.insert(newIndex, item);
-    });
-  }
 }
 
-class Dock extends StatefulWidget {
-  const Dock({
-    super.key,
-    required this.items,
-    required this.onReorder,
-  });
-
-  final List<IconData> items;
-  final Function(int, int) onReorder;
+class RedSquare extends StatefulWidget {
+  const RedSquare({super.key});
 
   @override
-  DockState createState() => DockState();
+  RedSquareState createState() => RedSquareState();
 }
 
-class DockState extends State<Dock> {
-  late List<IconData> _items;
-  late Map<String, List<Offset>> _itemPositionsMap;
-  Offset? _draggingPosition;
-  int? _draggingIndex;
-  final List<Offset> _dockPositions = [
-    const Offset(0, 100),
-    const Offset(75, 100),
-    const Offset(150, 100),
-    const Offset(225, 100),
-    const Offset(300, 100),
-  ];
+class RedSquareState extends State<RedSquare>
+    with SingleTickerProviderStateMixin {
+  double? _position;
+  bool _isAnimating = false;
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  void _moveRight(double screenWidth, double squareSize) {
+    if (_isAnimating) return;
+    setState(() {
+      _isAnimating = true;
+    });
+
+    double endPosition = screenWidth - squareSize;
+
+    _animation = Tween<double>(begin: _position ?? 0, end: endPosition)
+        .animate(_controller);
+
+    _controller.forward(from: 0.0);
+  }
+
+  void _moveLeft(double screenWidth, double squareSize) {
+    if (_isAnimating) return;
+    setState(() {
+      _isAnimating = true;
+    });
+
+    double endPosition = -screenWidth + squareSize;
+
+    _animation = Tween<double>(begin: _position ?? 0, end: endPosition)
+        .animate(_controller);
+
+    _controller.forward(from: 0.0);
+  }
 
   @override
   void initState() {
     super.initState();
-    _items = [
-      Icons.abc,
-      Icons.book,
-      Icons.call,
-      Icons.delete,
-      Icons.egg,
-    ];
-    _itemPositionsMap = {
-      'a': [const Offset(0, 100)],
-      'b': [const Offset(75, 100)],
-      'c': [const Offset(150, 100)],
-      'd': [const Offset(225, 100)],
-      'e': [const Offset(300, 100)],
-    };
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _controller.addListener(() {
+      setState(() {
+        _position = _animation.value;
+      });
+    });
+
+    _controller.addStatusListener((status) {
+      print("Animation Status: $status"); // Print the current status
+
+      if (status == AnimationStatus.completed ||
+          status == AnimationStatus.dismissed) {
+        print(
+            "Animation completed or dismissed."); // Print when the animation finishes
+
+        if (_isAnimating) {
+          print(
+              "Animation was in progress, setting _isAnimating to false."); // Print before setting the flag
+
+          if (mounted) {
+            setState(() {
+              _isAnimating = false; // Set the flag to false
+            });
+          }
+
+          print(
+              "State updated, _isAnimating is now: $_isAnimating"); // Print after setting the flag
+        } else {
+          print(
+              "Animation was not in progress, no need to set _isAnimating."); // If animation is not running
+        }
+      }
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _position = 0.0;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      width: double.infinity,
-      padding: const EdgeInsets.all(8),
-      child: Stack(
-        children: [
-          ...List.generate(
-            _items.length,
-            (index) {
-              return AnimatedPositioned(
-                duration: const Duration(milliseconds: 300),
-                left: _itemPositionsMap.containsKey(_getItemKey(index))
-                    ? _itemPositionsMap[_getItemKey(index)]![0].dx
-                    : 0.0,
-                top: _itemPositionsMap.containsKey(_getItemKey(index))
-                    ? _itemPositionsMap[_getItemKey(index)]![0].dy
-                    : 100.0,
-                child: GestureDetector(
-                  onPanUpdate: (details) {
-                    setState(() {
-                      _draggingIndex ??= index;
-                      _draggingPosition = details.localPosition;
-                    });
-                  },
-                  onPanEnd: (_) {
-                    setState(() {
-                      if (_draggingPosition != null) {
-                        final newIndex =
-                            _getClosestDockPosition(_draggingPosition!);
-                        final movedItem = _items.removeAt(_draggingIndex!);
-                        _items.insert(newIndex, movedItem);
-                        _shiftIcons(newIndex);
-                        _draggingPosition = null;
-                        _draggingIndex = null;
-                      }
-                    });
-                  },
-                  child: Container(
-                    height: 60,
-                    width: 60,
-                    decoration: BoxDecoration(
-                      color: Colors.primaries[index % Colors.primaries.length],
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Center(
-                      child: Icon(
-                        _items[index], // Use the _items list to get the icon
-                        color: Colors.white,
-                        size: 30, // Adjust size as needed
-                      ),
-                    ),
-                  ),
-                ),
-              );
-            },
+    double squareSize = 50.0;
+    double screenWidth = MediaQuery.sizeOf(context).width / 2 + squareSize / 2;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Transform.translate(
+          offset: Offset(_position ?? 0, 0),
+          child: Container(
+            width: squareSize,
+            height: squareSize,
+            color: Colors.red,
           ),
-          if (_draggingPosition != null && _draggingIndex != null)
-            Positioned(
-              left: _draggingPosition!.dx - 30,
-              top: _draggingPosition!.dy - 30,
-              child: Container(
-                height: 60,
-                width: 60,
-                decoration: BoxDecoration(
-                  color: Colors
-                      .primaries[_draggingIndex! % Colors.primaries.length],
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Center(
-                  child: Icon(_items[_draggingIndex!], color: Colors.white),
-                ),
-              ),
+        ),
+        const SizedBox(height: 50),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: !_isAnimating && (_position ?? 0) >= 0
+                  ? () {
+                      _moveLeft(screenWidth, squareSize);
+                    }
+                  : null,
+              child: const Text('To Left'),
             ),
-        ],
-      ),
+            const SizedBox(width: 20),
+            ElevatedButton(
+              onPressed: !_isAnimating && (_position ?? 0) <= 0
+                  ? () {
+                      _moveRight(screenWidth, squareSize);
+                    }
+                  : null,
+              child: const Text('To Right'),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
-  String _getItemKey(int index) {
-    return String.fromCharCode(97 + index);
-  }
+  @override
+  void dispose() {
+    _controller.removeListener(() {});
 
-  int _getClosestDockPosition(Offset position) {
-    double minDistance = double.infinity;
-    int closestIndex = 0;
-
-    for (int i = 0; i < _dockPositions.length; i++) {
-      double distance = (position.dx - _dockPositions[i].dx).abs() +
-          (position.dy - _dockPositions[i].dy).abs();
-      if (distance < minDistance) {
-        minDistance = distance;
-        closestIndex = i;
-      }
-    }
-
-    return closestIndex;
-  }
-
-  void _shiftIcons(int newIndex) {
-    if (_draggingIndex != null && _draggingPosition != null) {
-      final oldIndex = _draggingIndex!;
-      final draggedKey = _getItemKey(oldIndex);
-
-      final keys = _itemPositionsMap.keys.toList();
-      keys.removeAt(oldIndex);
-      keys.insert(newIndex, draggedKey);
-
-      for (int i = 0; i < keys.length; i++) {
-        final key = keys[i];
-        _itemPositionsMap[key] = [Offset(i * 75.0, 100.0)];
-      }
-
-      setState(() {});
-    }
+    _controller.dispose();
+    super.dispose();
   }
 }
