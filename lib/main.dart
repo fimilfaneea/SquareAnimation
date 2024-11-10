39e156d4e81435ab21e9268ed08ff21a323cf2cd
Fimil Faneea <fimilfaneea@gmail.com>
2024-11-10 21:10:18 +0530

    Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,217 +1,182 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MacOsInspiredDoc(),
-    );
-  }
-}
-
-/// A MacOS-style dock widget that allows for item dragging and reordering.
-class MacOsInspiredDoc extends StatefulWidget {
-  const MacOsInspiredDoc({super.key});
-
-  @override
-  State<MacOsInspiredDoc> createState() => _MacOsInspiredDocState();
-}
-
-class _MacOsInspiredDocState extends State<MacOsInspiredDoc> {
-  late int? hoveredIndex;
-  late double baseItemHeight;
-  late double baseTranslationY;
-  late double verticalItemsPadding;
-  String? draggedItem;
-
-  /// Determines the vertical translation (Y-axis) offset for an icon based on its index.
-  double getTranslationY(int index) {
-    return baseTranslationY;
-  }
-
-  /// Calculates the position offset for an icon when an item is dragged, to give
-  /// visual feedback for item reordering.
-  double getIconPosition(int index) {
-    if (draggedItem != null && hoveredIndex != null) {
-      if (index > hoveredIndex!) {
-        return -50.0;
-      } else if (index < hoveredIndex!) {
-        return 50.0;
-      }
-    }
-    return 0.0; // Default position when draggedItem is not hovering or outside
-  }
-
-  /// Returns the current width of the dock, shrinking it when an item is dragged
-  /// to simulate a "hovered" effect.
-  double getDockWidth() {
-    double baseDockWidth = baseItemHeight * items.length +
-        verticalItemsPadding * (items.length + 1);
-
-    if (draggedItem != null) {
-      baseDockWidth *= 0.8;
-    }
-
-    return baseDockWidth;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    hoveredIndex = null;
-    baseItemHeight = 80;
-    verticalItemsPadding = 20;
-    baseTranslationY = 0.0;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Positioned(
-              height: baseItemHeight,
-              left: 0,
-              right: 0,
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  gradient: const LinearGradient(
-                    colors: [Colors.blueAccent, Colors.greenAccent],
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: verticalItemsPadding),
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                width: getDockWidth(),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: List.generate(items.length, (index) {
-                    return Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: DragTarget<String>(
-                          onAcceptWithDetails: (details) {
-                            setState(() {
-                              items.remove(draggedItem);
-                              items.insert(index, draggedItem!);
-                              draggedItem = null;
-                            });
-                          },
-                          builder: (context, candidateData, rejectedData) {
-                            return Opacity(
-                              opacity: candidateData.isNotEmpty ? 0.5 : 1.0,
-                              child: MouseRegion(
-                                cursor: SystemMouseCursors.click,
-                                onEnter: (event) {
-                                  if (draggedItem != null) {
-                                    setState(() {
-                                      hoveredIndex =
-                                          index; // Update hoveredIndex only when an item is being dragged
-                                    });
-                                  }
-                                },
-                                onExit: (event) {
-                                  // Keep hoveredIndex unchanged, so the last hovered index stays
-                                },
-                                child: Draggable<String>(
-                                  data: items[index],
-                                  onDragStarted: () {
-                                    setState(() {
-                                      draggedItem = items[index];
-                                    });
-                                  },
-                                  onDragUpdate: (details) {
-                                    setState(() {
-                                      final itemWidth =
-                                          MediaQuery.of(context).size.width /
-                                              items.length;
-                                      hoveredIndex =
-                                          (details.localPosition.dx /
-                                                  itemWidth)
-                                              .floor();
-                                    });
-                                  },
-                                  onDraggableCanceled: (velocity, offset) {
-                                    setState(() {
-                                      draggedItem = null;
-                                    });
-                                  },
-                                  childWhenDragging: SizedBox(
-                                    height: baseItemHeight,
-                                    width: baseItemHeight,
-                                  ),
-                                  feedback: Material(
-                                    color: Colors.transparent,
-                                    child: Container(
-                                      width: 80,
-                                      height: 80,
-                                      alignment: Alignment.center,
-                                      child: Text(
-                                        items[index],
-                                        style: const TextStyle(fontSize: 40),
-                                      ),
-                                    ),
-                                  ),
-                                  child: AnimatedContainer(
-                                    duration: const Duration(seconds: 1),
-                                    transform: Matrix4.translationValues(
-                                      getIconPosition(index),
-                                      getTranslationY(index),
-                                      0,
-                                    ),
-                                    child: FittedBox(
-                                      fit: BoxFit.contain,
-                                      child: Text(
-                                        items[index],
-                                        style: const TextStyle(
-                                          fontSize: 40,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                ),
-              ),
-            ),
-          ],
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: RedSquare(),
         ),
       ),
     );
   }
 }
 
+class RedSquare extends StatefulWidget {
+  const RedSquare({super.key});
 
-/// List of items displayed in the dock as draggable icons.
-List<String> items = [
-  '📱', 
-  '💻', 
-  '💾', 
-  '🔋', 
-  '⚙️', 
-];
+  @override
+  RedSquareState createState() => RedSquareState();
+}
+
+class RedSquareState extends State<RedSquare> with SingleTickerProviderStateMixin {
+  /// The current position of the red square on the x-axis.
+  double? _position;
+
+  /// Indicates if the square is currently animating.
+  bool _isAnimating = false;
+
+  /// Disables the button when the square is moving in one direction.
+  bool _isButtonDisabled = false;
+
+  /// The controller that drives the animation.
+  late AnimationController _controller;
+
+  /// The animation that moves the square.
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initializes the animation controller with a duration and vsync.
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    // Updates the position of the square as the animation progresses.
+    _controller.addListener(() {
+      setState(() {
+        _position = _animation.value;
+      });
+    });
+
+    // Listens for changes in the animation status.
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.forward) {
+        // Indicates that the animation is in progress.
+        if (mounted) {
+          setState(() {
+            _isAnimating = true;
+          });
+        }
+      }
+
+      // Resets the animation status when completed or dismissed.
+      if (status == AnimationStatus.completed || status == AnimationStatus.dismissed) {
+        if (_isAnimating) {
+          if (mounted) {
+            setState(() {
+              _isAnimating = false;
+            });
+          }
+        }
+      }
+    });
+  }
+
+  /// Moves the red square to the right edge of the screen.
+  ///  
+  /// Parameters:
+  /// - [screenWidth]: The width of the screen.
+  /// - [squareSize]: The size of the red square.
+  void _moveRight(double screenWidth, double squareSize) {
+    if (_isAnimating) return;
+    setState(() {
+      _isAnimating = true;
+    });
+
+    double endPosition = screenWidth - squareSize;
+    _animation = Tween<double>(begin: _position ?? 0, end: endPosition).animate(_controller);
+    _controller.forward(from: 0.0);
+  }
+
+  /// Moves the red square to the left edge of the screen and disables the button temporarily.
+  /// 
+  /// Parameters:
+  /// - [screenWidth]: The width of the screen.
+  /// - [squareSize]: The size of the red square.
+  void _moveLeft(double screenWidth, double squareSize) {
+    if (_isAnimating) return;
+    setState(() {
+      _isAnimating = true;
+      _isButtonDisabled = true;
+    });
+
+    double endPosition = -screenWidth + squareSize;
+    _animation = Tween<double>(begin: _position ?? 0, end: endPosition).animate(_controller);
+    _controller.forward(from: 0.0);
+
+    // Enables the button after the animation completes.
+    Future.delayed(const Duration(seconds: 1), () {
+      if (mounted) {
+        setState(() {
+          _isButtonDisabled = false;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double squareSize = 50.0;
+    double screenWidth = MediaQuery.sizeOf(context).width / 2 + squareSize / 2;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform.translate(
+              offset: Offset(_position ?? 0, 0),
+              child: child,
+            );
+          },
+          child: Container(
+            width: squareSize,
+            height: squareSize,
+            color: Colors.red,
+          ),
+        ),
+        const SizedBox(height: 50),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: !_isAnimating && (_position ?? 0) >= 0 && !_isButtonDisabled
+                  ? () {
+                      _moveLeft(screenWidth, squareSize);
+                    }
+                  : null,
+              child: const Text('To Left'),
+            ),
+            const SizedBox(width: 20),
+            ElevatedButton(
+              onPressed: !_isAnimating && (_position ?? 0) <= 0 && !_isButtonDisabled
+                  ? () {
+                      _moveRight(screenWidth, squareSize);
+                    }
+                  : null,
+              child: const Text('To Right'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    // Disposes of the animation controller to free resources.
+    _controller.dispose();
+    super.dispose();
+  }
+}
